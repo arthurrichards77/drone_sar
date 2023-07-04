62bc9e122b41196f92a36c3edcd716d2976f2671
Arthur Richards <aeagr@bristol.ac.uk>
2023-07-04 23:02:03 +0100

    Now with function toolbox including MISPER and POI additions

--- tracker_map.py
+++ tracker_map.py
@@ -8,6 +8,8 @@ from matplotlib.figure import Figure
 
 import pyproj
 
+import argparse
+
 crs_osgb = pyproj.CRS.from_epsg(27700)
 lat_lon_to_east_north = pyproj.Transformer.from_crs(crs_osgb.geodetic_crs, crs_osgb)
 
@@ -40,31 +42,77 @@ class TkTrackerMap(FigureCanvasTkAgg):
         base_map = rasterio.open(tile_file_name)
         show(base_map, ax=self.ax)
         self.tile_limits = self.ax.axis()
+        self.tracks = []
 
     def add_track(self,name, track_style='-', head_style='x'):
-        return MapTrack(name, self, track_style, head_style)
+        new_track = MapTrack(name, self, track_style, head_style)
+        self.tracks.append(new_track)
+        return new_track
     
 class TrackerApp:
 
-    def __init__(self):
+    def __init__(self, tile_file_name, mav_connect_str):
         self.root = tkinter.Tk()
         self.root.wm_title("Tracker Map")
         self.tracker_map = TkTrackerMap(self.root, 'llanbedr_rgb.tif')
-        self.doodle_track = self.tracker_map.add_track('Doodle')
+        self.misper_track = self.tracker_map.add_track('MISPER')
+        self.poi_tracks = []
+        # click event handler and toolbar work together
+        self.click_mode = None
         self.tracker_map.mpl_connect("button_press_event", self.click_handler)
+        track_toolbar = self.build_track_toolbar()
+        # include built-in toolbar for map zoom and pan etc
+        self.nav_toolbar = NavigationToolbar2Tk(self.tracker_map, self.root, pack_toolbar=False)
+        self.nav_toolbar.update()
+        # assemble the GUI
+        self.nav_toolbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
+        track_toolbar.pack(side=tkinter.TOP)
         self.tracker_map.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
+
+    def set_click_mode(self, new_mode):
+        self.click_mode = new_mode
+        if new_mode:
+            nav_state = tkinter.DISABLED
+        else:
+            nav_state = tkinter.NORMAL
+        for btn in self.nav_toolbar._buttons:
+            self.nav_toolbar._buttons[btn]['state'] = nav_state
+
+    def click_handler(self,e):
+        if self.click_mode=='MISPER':
+            self.misper_track.update(e.xdata,e.ydata)
+        elif self.click_mode=='POI':
+            next_poi = len(self.poi_tracks)+1
+            new_poi = self.tracker_map.add_track(f'POI{next_poi}', head_style='b^')
+            self.poi_tracks.append(new_poi)
+            new_poi.update(e.xdata,e.ydata)
+        self.tracker_map.draw()
+
+    def build_track_toolbar(self):
+        toolbar_frame = tkinter.Frame(master=self.root)
+        nav_button = tkinter.Button(master=toolbar_frame, text='NAV', command=lambda: self.set_click_mode(None))
+        nav_button.grid(row=0,column=0)
+        misper_button = tkinter.Button(master=toolbar_frame, text='MISPER', command=lambda: self.set_click_mode('MISPER'))
+        misper_button.grid(row=0,column=1)
+        poi_button = tkinter.Button(master=toolbar_frame, text='POI', command=lambda: self.set_click_mode('POI'))
+        poi_button.grid(row=0,column=2)
+        return toolbar_frame
 
     def run(self):
         self.root.mainloop()
 
-    def click_handler(self,e):
-        self.doodle_track.update(e.xdata,e.ydata)
-        self.tracker_map.draw()
-
 def main():
-    
-    app = TrackerApp()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--tile_file',
+                        help='Path to GeoTIFF file for basemap',
+                        default='llanbedr_rgb.tif')
+    parser.add_argument('-c', '--connect',
+                        help='Connection string e.g. tcp:localhost:14550',
+                        default=None)
+    args = parser.parse_args()
+    app = TrackerApp(args.tile_file, args.connect)
     app.run()
+
 
 if __name__=='__main__':
     main()
