3291389f34a28eb4c1b84f196af20c7885cf3424
Arthur Richards <aeagr@bristol.ac.uk>
2023-07-04 17:38:31 +0100

    Adding zoom controls.

--- embedding_in_tk_sgskip.py
+++ embedding_in_tk_sgskip.py
@@ -11,6 +11,8 @@ import numpy as np
 
 from pymavlink import mavutil
 
+from math import sqrt
+
 import pyproj
 
 crs_osgb = pyproj.CRS.from_epsg(27700)
@@ -29,7 +31,6 @@ drone_line, = ax.plot([p[0] for p in drone_track],[p[1] for p in drone_track],'g
 drone_marker, = ax.plot([],[],'gx')
 poi_line, = ax.plot([p[0] for p in poi_list],[p[1] for p in poi_list],'b^')
 
-
 connect_str = 'tcp:localhost:5762'
 mav_connection = mavutil.mavlink_connection(connect_str)
 print(f'Connected to {connect_str}')
@@ -43,7 +44,6 @@ def timer_loop():
     msg = mav_connection.recv_match(type=['HEARTBEAT',
                                           'GLOBAL_POSITION_INT'
                                                ], blocking=False)
-    print(num_loops)
     if num_loops>1000:
         if msg:
             mav_connection.mav.request_data_stream_send(msg.get_srcSystem(),
@@ -78,8 +78,16 @@ canvas.mpl_connect("button_press_event", click_handler)
 var_mouse = tkinter.StringVar(master=root, value='Current coordinates')
 label_mouse = tkinter.Label(master=root, textvariable=var_mouse)
 
+var_dist = tkinter.StringVar(master=root, value="Distances\nDrone:\t0.0")
+label_dist = tkinter.Label(master=root, textvariable=var_dist)
+
+def distance(x1,y1,x2,y2):
+    return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
+
 def hover_handler(e):
-    var_mouse.set(f'{e.xdata},{e.ydata}')
+    if drone_track:
+        d = distance(e.xdata,e.ydata,drone_track[-1][0],drone_track[-1][1])
+        var_dist.set(f'Distances\nDrone\t{int(d)}')
 
 canvas.mpl_connect("motion_notify_event", hover_handler)
 
@@ -95,7 +103,31 @@ def zoom_in():
     ax.axis([xmin,xmax,ymin,ymax])
     canvas.draw()
 
-button_zoom = tkinter.Button(master=root, text="Zoom In", command=zoom_in)
+def pan_right():
+    limits = ax.axis()
+    pan_factor = 0.3
+    xmin = limits[0] + int(pan_factor*(limits[1] - limits[0]))
+    xmax = limits[1] + int(pan_factor*(limits[1] - limits[0]))
+    if xmax > tile_limits[1]:
+        xmin = tile_limits[1] - (xmax-xmin)
+        xmax = tile_limits[1]
+    ax.axis([xmin,xmax,limits[2],limits[3]])
+    canvas.draw()
+
+def zoom_reset():
+    ax.axis(tile_limits)
+    canvas.draw()
+
+frame_zoom = tkinter.Frame(master=root)
+
+button_zoom_in = tkinter.Button(master=frame_zoom, text="Zoom In", command=zoom_in)
+button_zoom_in.grid(column=0,row=0)
+
+button_zoom_reset = tkinter.Button(master=frame_zoom, text="Reset Zoom", command=zoom_reset)
+button_zoom_reset.grid(column=1,row=1)
+
+button_pan_right = tkinter.Button(master=frame_zoom, text="Pan right", command=pan_right)
+button_pan_right.grid(column=2,row=1)
 
 button_quit = tkinter.Button(master=root, text="Quit", command=root.destroy)
 
@@ -105,8 +137,9 @@ button_quit = tkinter.Button(master=root, text="Quit", command=root.destroy)
 # sure the UI controls are displayed as long as possible.
 button_quit.pack(side=tkinter.BOTTOM)
 #toolbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
-label_mouse.pack(side=tkinter.TOP)
-button_zoom.pack(side=tkinter.RIGHT)
+#label_mouse.pack(side=tkinter.TOP)
+label_dist.pack(side=tkinter.TOP)
+frame_zoom.pack(side=tkinter.RIGHT)
 canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
 
 timer_loop()
