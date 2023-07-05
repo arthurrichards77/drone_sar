66f30cbd590281a3d46b1356ec7790742b2aff46
Arthur Richards <aeagr@bristol.ac.uk>
2023-07-05 14:24:24 +0100

    Tidy up of old test materials

--- chat_server.py
+++ chat_server.py
@@ -6,33 +6,6 @@ app = Flask(__name__)
 
 msgs = []
 
-@app.route("/hello/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/loc_test/")
-def loc_test():
-    return render_template('loc_test.html')
-
-@app.route("/form_test/")
-def form_test():
-    return render_template('form_test.html',name=None)
-
-@app.route("/result_test/")
-def result_test():
-    name = request.args.get('name',None)
-    print(name)
-    return render_template('form_test.html',name=name)
-
-@app.route("/post_test/")
-def post_test():
-    return render_template('post_test.html',name=None)
-
-@app.route("/post_result/", methods=['GET', 'POST'])
-def post_result():
-    name = request.form['name']
-    return render_template('post_test.html',name=name)
-
 @app.route("/")
 @app.route("/chat/", methods=['GET', 'POST'])
 def chat():
